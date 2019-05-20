52f120f0aa7d62bedbd1a43b0282037dd1772e01
Ruairi-osul <rorylikesmusic@gmail.com>
2019-05-20 17:38:50 +0100

    post_ks

--- pp/post_ks_classes.py
+++ pp/post_ks_classes.py
@@ -1,9 +1,16 @@
 import numpy as np
+import pandas as pd
 
 
 class SpikeSortedRecording:
     '''
-    Initialised with path to 
+    path should be a pathlib object
+    Usage:
+        processor = SpikeSortedRecording(path, extracted)
+        processir.extract_spiketimes()
+        processor.extract_waveforms()
+        processor.calculate_ifr()
+        processor.get_trail_sampl
 
     methods:
         Get
@@ -14,16 +21,34 @@ class SpikeSortedRecording:
         self.path = path
         self.nchans = nchans
         self.extracted = extracted
-        self.raw_data = self.load_data()
+        self.raw_data = self.load_raw_data()
+        self.good_clusters = self.get_cluster_group_ids(group='good')
+        self.mua_clusters = self.get_cluster_group_ids(group='mua')
+        self.spike_times = self.load_spike_times()
+        self.load_spike_clusters = load_spike_clusters
 
-    def load_data(self):
+    def load_raw_data(self):
         tmp = np.memmap(self.path)
         shape = int(len(tmp) / self.nchans)
         return np.memmap(self.path, dtype=np.int,
                          shape=(shape, self.nchans))
 
-    def get_good_clusters(self):
-        pass
+    def get_cluster_group_ids(self, group):
+        cluster_groups = pd.read_csv(
+            self.path.joinpath('cluster_groups.csv'), sep='\t')
+        cluster_groups = cluster_groups.loc[cluster_groups['group'] == group, :]
+        return cluster_groups['cluster_id'].values
+
+    def load_spike_times(self):
+        return np.load(self.path.joinpath('spike_times.npy'))
+
+    def load_spike_clusters(self):
+        return np.load(self.path.joinpath('spike_clusters.npy'))
+
+    def get_cluster_spiketimes(self, clusters):
+        df = pd.DataFrame({'cluster_id': self.spike_clusters.flatten(),
+                           'spike_times': self.spike_times.flatten()})
+        return df.loc[df['cluster_id'].isin(clusters), :]
 
     def get_waveforms(self):
         pass
